7ffa5c51b2cd075d572c81e4c671fdd162cc69a5
Joel Peter Sony <pete.devs14@gmail.com>
2024-10-05 17:37:18 +0530

    Update Test Page

--- focused-youtube-carousel/app/switchless/test/page.jsx
+++ focused-youtube-carousel/app/switchless/test/page.jsx
@@ -1,25 +1,75 @@
 "use client"
-// import { Link } from "@mui/joy";
-import { Button } from "@mui/joy";
-import LinkButton from "../_components/LinkButton";
-import LinkV2 from "../_components/LinkV2";
+import { Button,Link } from "@mui/joy";
 import Box from "@mui/joy/Box";
+import TabNavigation from "../_components/TabPanel";
+import Logo from "../_components/LogoV1";
+import LogoV2 from "../_components/Logo";
+import TableData from "../_components/TableData";
+import CustomTable from "../_components/Table";
 
-import TabPanel from "../_components/TabPanel";
-function page() {
+function Page() {
+  const { PORs } = TableData;
+
+const transformPORsToTableData = (PORs) => {
+    return PORs.map((item) => [
+      <Link
+        key={item.id}
+        sx={{ textDecoration: "none", "&:hover": { textDecoration: "none" } }}
+        href={`/org/${item.org}/por/po_request/${item.id}`}
+      >
+        <span style={{ whiteSpace: "nowrap" }}>{item.remote_id}</span>
+      </Link>,
+      item.vendor.name,
+      item.budget.name,
+      <span key={item.id} style={{ whiteSpace: "nowrap" }}>something</span>,
+      <span
+        key={item.id}
+        style={{
+          backgroundColor:"red",
+          color: "white",
+          padding: '4px 8px',
+          borderRadius: '4px',
+          display: 'inline-block',
+        }}
+      >
+        {item.status}
+      </span>,
+      item.payment_process,
+      <span key={item.id} style={{ whiteSpace: "nowrap" }}>{`${item.currency} ${item.basic_total}`}</span>,
+      <span key={item.id} style={{ whiteSpace: "nowrap" }}>{`${item.currency} ${item.grand_total}`}</span>,
+      item.submitted_by.name,
+      item.is_paid_out ? 
+        <span key={item.id} style={{ color: "#2c662d", backgroundColor: "#fcfff5", padding: '4px 8px', borderRadius: '4px' }}>paid</span> :
+        <span key={item.id} style={{ color: "#9f3a38", backgroundColor: "#fff6f6", padding: '4px 8px', borderRadius: '4px' }}>not paid</span>,
+      <Box
+        key={item.id}
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        {item.documents.length ? (
+          <>
+            
+            {item.documents.length}
+          </>
+        ) : (
+          0
+        )}
+      </Box>,
+    ]);
+  }
+  const tableBodyData = transformPORsToTableData(TableData.PORs);
+  const headers = ["PO Request Number", "Vendor", "Budget", "Date", "Status", "Payment Process", "PO Basic Total", "PO Grand Total", "Submitted By", "Is paid out", ""];
+
+
   return (
     <>
-    <h1>Test Page</h1>
-
-    {/* <LinkV2 target="_self"
-    Component={()=> <Button>Home</Button>}
-     href="/switchless/">This is  overriden</LinkV2>
-    <Link href="/switchless/">Home</Link> */}
-    <Box sx={{width: "30vw", height: "30vh"}}>
-    <TabPanel />
-    </Box>
+      <h1>Test Page</h1>
+  
+     
+        <Logo/>
+        <CustomTable headers={headers} data={tableBodyData} />
+      {/* </Box> */}
     </>
   )
 }
 
-export default page
+export default Page
---
+++ focused-youtube-carousel/app/switchless/test2/page.jsx
@@ -0,0 +1,22 @@
+"use client"
+import { Button } from "@mui/joy";
+import Box from "@mui/joy/Box";
+import StateFullTabs2 from "../_components/StateFullTabs2";
+import Logo from "../_components/LogoV1";
+
+
+
+function Page() {
+  return (
+    <>
+      <h1>Test Page</h1>
+      <Box sx={{width: "30vw", height: "30vh"}}>
+        {/* {/* <StateFullTabs2 tabData={tabData1} tabPanels={tabPanels1} tabGroupId="group1" /> 
+        {/* <StateFullTabs2 tabData={tabData2} tabPanels={tabPanels2} tabGroupId="group2" /> *
+        <Logo />
+      </Box>
+    </>
+  )
+}
+
+export default Page
