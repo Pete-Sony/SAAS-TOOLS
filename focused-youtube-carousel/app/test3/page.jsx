c664143092ae024465ecc1b04130c05f1af2909f
Joel Peter Sony <pete.devs14@gmail.com>
2024-10-06 09:52:18 +0530

    Update Test Page

---
+++ focused-youtube-carousel/app/test3/page.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import TestClickToEdit from './ClickToEdit/TestClickToEdit'
+function page() {
+  return (
+    <div>
+        <TestClickToEdit/>
+    </div>
+  )
+}
+
+export default page
