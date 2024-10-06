12c68eb575867b77b081e483cb6c266bab0c6910
Joel Peter Sony <pete.devs14@gmail.com>
2024-10-06 17:39:51 +0530

    Add Docs

--- focused-youtube-carousel/app/test3/ClickToEdit/ClickToEdit.jsx
+++ focused-youtube-carousel/app/test3/ClickToEdit/ClickToEdit.jsx
@@ -1,19 +1,41 @@
 'use client'
 
-import { useState } from 'react';
-import {Link} from "@mui/joy";
+import { useState, useRef } from 'react';
+import {Link,Input} from "@mui/joy";
+import EditIcon from '@mui/icons-material/Edit';
 
  export default function ClickToEdit({children}) {
   const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(children);
+  const inputRef = useRef(null);
   const handleClick = ()=>{ 
     setIsEditing(!isEditing);
-  }
-  return (<>
-  {isEditing ?
-   <input onClick={handleClick} type="text" value={children} />
-    :
-   <Link onClick={handleClick}>{children}</Link> }
+    setTimeout(() => {
+      if(inputRef.current){
+
+        inputRef.current.focus();
+        inputRef.current.setSelectionRange(0, inputRef.current.value.length);
+      }
+      
+    }, 0);
+  
  
+  }
+  const handleChange = (e)=>{
+    setInputValue(e.target.value);
+  }
+  const renderCount = useRef(0);
+  renderCount.current++;
+  return (
+  <>
+  {isEditing ?
+   <Input   type="text" value={inputValue} onChange={handleChange} />
+    :
+   <Link onClick={handleClick} endDecorator={<EditIcon/>} >{children}</Link> }
+   {/* <div>
+  { `I've rendered this component ${renderCount.current} times` }
+   </div>
+  */}
   </>
 
  
