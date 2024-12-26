dc0ef59c35aa8b5f22e00d1388f1cbdf2dea7ba2
Joel Peter Sony <pete.devs14@gmail.com>
2024-12-26 19:03:58 +0530

    Clean Up
    Removed global Css

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+
 import YouTubeIframe from "./YouTubeIframe";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div >
       <h1>So may we Start?</h1>
       <YouTubeIframe/>
       {/* <main className={styles.main}>
