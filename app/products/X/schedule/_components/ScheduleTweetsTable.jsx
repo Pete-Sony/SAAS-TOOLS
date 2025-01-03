aeb98f7fc2d3af9b4aa05891cc95b7478c17e31c
Pete <pete.devs14@gmail.com>
2025-01-04 02:01:17 +0530

    (feat) : ScheduleTweetsTable

---
+++ app/products/X/schedule/_components/ScheduleTweetsTable.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Table } from '@mui/joy'
+
+export default function ScheduleTweetsTable() {
+  return (
+
+<Table>
+  <thead>
+    <tr>
+
+    <th>Tweets</th>
+    </tr>
+
+  </thead>
+  <tbody>
+    <tr><td>This is a table</td></tr>
+    <tr><td>This is where we will</td></tr>
+    <tr><td>Queue up the tweets we Schedule</td></tr>
+    <tr><td>Additional Features Oncoming</td></tr>
+    
+
+    
+
+  </tbody>
+
+</Table>
+  )
+}
--- app/products/X/schedule/page.jsx
+++ app/products/X/schedule/page.jsx
@@ -1,8 +1,19 @@
+import { Typography,Box } from '@mui/joy'
 import React from 'react'
+import ScheduleTweetsTable from './_components/ScheduleTweetsTable'
 
 function Schedule() {
   return (
-    <div>Schedule</div>
+    <>
+    <Box sx={{mt:2,mb:2,p:1,
+      // border:"2px solid red"
+      }}>
+     <Typography level='h3'>Schedule Tweets</Typography>
+    </Box>
+
+    <ScheduleTweetsTable/>
+    {/* <div>Schedule</div> */}
+    </>
   )
 }
 
