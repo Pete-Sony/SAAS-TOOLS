8f53c0384a6fd2130108c20e5ee0b2cb20487765
Pete Sony <pete.devs14@gmail.com>
2025-01-07 10:17:27 +0000

    Integrate supabase and it's library functions

--- app/products/X/schedule/_components/CreateTweet.jsx
+++ app/products/X/schedule/_components/CreateTweet.jsx
@@ -1,9 +1,17 @@
+'use client'
 import React from 'react'
 import Button from '@mui/joy/Button';
 import { Add } from '@mui/icons-material';
 
 export default function CreateTweet() {
+ 
+
+  const handleClick =()=>{
+
+  }
+
+
   return (
-    <Button startDecorator={<Add/>} variant="soft" >Create Tweet</Button>
+    <Button startDecorator={<Add/>} variant="soft" onClick={handleClick} >Create Tweet</Button>
   )
 }
