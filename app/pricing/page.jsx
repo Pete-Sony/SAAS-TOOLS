b8dd514b1e161312e50473741980de8370a1831d
Pete Sony <pete.devs14@gmail.com>
2025-01-16 22:06:24 +0000

    chore: Update Next.js version and install dependencies

--- app/pricing/page.jsx
+++ app/pricing/page.jsx
@@ -1,6 +1,8 @@
 'use client'
 import React from 'react'
 import TabNavigationHeader from '../_components/TabNavigationHeader'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 
 export default function page() {
