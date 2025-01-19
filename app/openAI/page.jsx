7c5e41bb37c3eadad3c18ed7fe2408fc46880ca5
Prithvij2004 <manikandan2200j@gmail.com>
2025-01-19 11:21:34 +0530

    (feat): generate tweet

--- app/openAI/page.jsx
+++ app/openAI/page.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
-import { generateHaiku } from './actions'
+"use client"
+import React, { useState } from 'react'
 import { Button, Card, Typography } from '@mui/joy'
+import { generateTweets } from './actions'
 
 export default function Page() {
+  const [tweets, setTweets] = useState([]);
+
+  const handleClick = async () => {
+    alert("Generating Tweets")
+    const response = await generateTweets("AI Agents taking over the world")
+    setTweets(response.tweets)
+  }
+
   return (
     <>
      <Card sx={{
         display:"flex",
+        flexDirection:"column",
+        gap:2,
         justifyContent:"center",
         alignItems:"center",
         height:"100vh"
         }}
-        >
+      >
         <Typography>Open AI</Typography>
-            <Button
-            // onClick={generateHaiku}
-            >Generate Haiku
-            </Button>
-        </Card>
+        <Button
+          onClick={handleClick}
+        >Generate Tweets
+        </Button>
+        
+        {tweets.length > 0 && tweets.map((tweet, index) => (
+          <Typography key={index} sx={{ mt: 1 }}>
+            {tweet.tweet}
+          </Typography>
+        ))}
+      </Card>
     </>
   )
 }
