5d961207737719e864fefe2863d68865ab70f08b
Prithvij2004 <manikandan2200j@gmail.com>
2025-01-23 10:07:15 +0530

    (fix): Fix env leak

--- app/products/clinic/model.js
+++ app/products/clinic/model.js
@@ -1,7 +1,7 @@
 import pg from 'pg'
 import { DataTypes, Sequelize } from "sequelize"
 
-const sequelize = new Sequelize('postgresql://postgres.wqcffldlsqeoshbcobzg:swedenclinic2025@aws-0-ap-south-1.pooler.supabase.com:5432/postgres', {
+const sequelize = new Sequelize(process.env.SUPABASE_URL, {
   dialect: 'postgres',
   dialectModule: pg,
   logging: false, // Disable logging (optional)
@@ -41,9 +41,7 @@ const Appointment = sequelize.define('Appointment', {
 
 export const initDB = async () => {
   try {
-    console.log("reached point 1")
     await sequelize.authenticate();
-    console.log("reached point 2")
     await Appointment.sync();
     console.log('Database connected and model synced');
   } catch (error) {
